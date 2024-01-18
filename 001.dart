23b8beb442ae74381f5e7fe7c159a567ae9cf776
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2024-01-18 12:22:12 +0530

    Add files via upload

---
+++ 001.dart
@@ -0,0 +1,8 @@
+void main(){
+  
+  print("kaushik"); //Print is keyword that will output whatever you want o debug console.
+//A statement is a command, something you tell the computerto do. In Dart, all simple statements end with a semicolon.You’ve already seen that with the print statement.A dart has its complex statement also.No semicolon is need for the opeing and closing of curly braces. Expression is somethhing that can be calcules as value(text,number or any other type)
+print("2+4");
+print("72-8");
+print ("58/2");
+}
