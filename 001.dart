840f191a1e359536f7a7b88c3655ee64411b17b6
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2024-01-18 14:05:20 +0530

    Update 001.dart

--- 001.dart
+++ 001.dart
@@ -7,5 +7,14 @@ print("2+4");
 print("72-8");
 print ("58/2");
   print(max(5,10));
+ print( 10.isEven);
+ num myNumber;
+ myNumber=4;
+ myNumber=2.04;
+ int myNumber1;
+ myNumber1=5;
+ //myNumber1=5.12; This line will give error as Dart is typesafe language which means once you tell Dart what varibale types is ,you camt change it later. 
+ print(myNumber);
+  print(myNumber1)
  
 }
