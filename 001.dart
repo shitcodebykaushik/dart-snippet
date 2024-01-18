5fe94d9a87df07fa2f1226d41ff9d53d078565d4
kaushik raj <122168016+shitcodebykaushik@users.noreply.github.com>
2024-01-18 19:28:16 +0530

    Update 001.dart

--- 001.dart
+++ 001.dart
@@ -15,6 +15,47 @@ print ("58/2");
  myNumber1=5;
  //myNumber1=5.12; This line will give error as Dart is typesafe language which means once you tell Dart what varibale types is ,you camt change it later. 
  print(myNumber);
-  print(myNumber1)
- 
+  print(myNumber1);
+ //Dart sring are the collection of the UTF-16 code units.It doesnt take the value as the char.
+ const kaushik='kaushik';
+  print(kaushik.codeUnits);
+  const dart="hi";
+  print(dart.codeUnits);
+ var flaf="Apple";
+  print(flaf.codeUnits.length);
+  var flaf1="ANT";
+  print(flaf1.runes.length);
+ //Adding two string in one is known as concetation.
+   var message ="kaushik "+" raj";
+  const name =" is  boy";
+  
+  print(message + name);
+/// These three ''' stand for the multiline .
+ const kau='''
+kaushik is 
+a boy.''';
+print(kau);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 }
